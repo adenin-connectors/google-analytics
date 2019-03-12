0021bf397486b5664f1284ef6a654c79f5a4db4d
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-12 10:08:51 +0100

    edited 'ping.js' activity.

--- activities/ping.js
+++ activities/ping.js
@@ -6,7 +6,7 @@ const api = require('./common/api');
 module.exports = async (activity) => {
   try {
     api.initialize(activity);
-    const response = await api('/user');
+    const response = await api('/analytics/v3/data/ga?metrics=ga:users&start-date=today&end-date=today');
 
     activity.Response.Data = {
       success: response && response.statusCode === 200
